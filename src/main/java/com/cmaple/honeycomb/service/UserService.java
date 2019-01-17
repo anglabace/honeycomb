036aca195cce24ae60c3f4712ebfad1266fecacf
C-Maple <congfeng12@163.com>
2019-01-17 17:36:08 +0800

    更新用户查询函数

--- src/main/java/com/cmaple/honeycomb/service/UserService.java
+++ src/main/java/com/cmaple/honeycomb/service/UserService.java
@@ -48,12 +48,30 @@ public class UserService extends ServiceImpl<UserMapper, User> {
     }
 
     /**
-     * 函数名：查询函数-根据用户名、密码查询用户信息 - getUser（）
-     * 功能描述： 根据账户名、密码获取当前用户的相关信息
+     * 函数名：查询函数-根据用户名、密码查询用户是否存在 - hasByUsernameAndPassword（）
+     * 功能描述： 根据账户名、密码获取当前用户是否存在
      * 输入参数：<按照参数定义顺序>
      *
      * @param username String类型的用户名
      * @param password String类型的用户密码
+     *                 返回值：int
+     *                 异    常：无
+     *                 创建人：CMAPLE
+     *                 日期：2019-01-17
+     *                 修改人：
+     *                 级别：普通用户
+     *                 日期：
+     */
+    public int hasByUsernameAndPassword(String username, String password) {
+        return userMapper.selectCount(new QueryWrapper<User>().lambda().eq(User::getUsername, username).eq(User::getPassword, password));
+    }
+
+    /**
+     * 函数名：查询函数-根据用户名查询用户信息 - getUserByUsername（）
+     * 功能描述： 根据用户名查询用户信息
+     * 输入参数：<按照参数定义顺序>
+     *
+     * @param username String类型的用户名
      *                 返回值：User
      *                 异    常：无
      *                 创建人：CMAPLE
@@ -62,8 +80,8 @@ public class UserService extends ServiceImpl<UserMapper, User> {
      *                 级别：普通用户
      *                 日期：
      */
-    public User getUser(String username, String password) {
-        return userMapper.selectOne(new QueryWrapper<User>().lambda().eq(User::getUsername, username).eq(User::getPassword, password));
+    public User getUserByUsername(String username) {
+        return userMapper.selectOne(new QueryWrapper<User>().lambda().eq(User::getUsername, username));
     }
 
     /**
